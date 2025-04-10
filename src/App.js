a1a52750675ca8c98e951522f7c020fc8c5a69cd
Nguyễn Thanh Sang <sangnt6604@gmail.com>
2025-04-10 10:46:47 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+  }
+
+  handleClick = (value) => {
+    const { input } = this.state;
+    if (value === 'C')
+      this.setState({ input: '' });
+    else if (value === '=') {
+      this.setState({ input: eval(input).toString() });
+    } else if (value === 'DEL')
+      this.setState({ input: input.slice(0, -1) });
+    else
+      this.setState({ input: input + value });
+  };
+
+  render() {
+    const buttons = [
+      '(', ')', 'C', 'DEL',
+      '7', '8', '9', '/',
+      '4', '5', '6', '*',
+      '1', '2', '3', '-',
+      '0', '.', '=', '+'
+    ];
+
+    const getClass = (btn) => {
+      if (['/', '*', '-', '+', '='].includes(btn)) return 'operator';
+      if (['(', ')', 'DEL', 'C'].includes(btn)) return 'secondary';
+      return '';
+    };
+
+    return (
+      <div className="calculator">
+        <div className="display">{this.state.input || '0'}</div>
+        <div className="buttons">
+          {buttons.map((btn, idx) => (
+            <button
+              key={idx}
+              className={getClass(btn)}
+              onClick={() => this.handleClick(btn)}
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
